87bfc55bad846c611dd3a209667d31c92256604a
jaeyoon <chris0258@naver.com>
2025-07-31 14:11:56 +0900

    green p.42 refac p.49

--- game.py
+++ game.py
@@ -1,11 +1,21 @@
 class Game:
     def guess(self, guess_number):
+        self.assert_illegal_value(guess_number)
+
+    def assert_illegal_value(self, guess_number):
         if guess_number is None:
-            raise TypeError()
+            raise TypeError("입력이 None 임")
 
         if len(guess_number) != 3:
-            raise TypeError()
+            raise TypeError("입력 문자열 길이가 3 이 아님")
 
-        for number in guess_number:
-            if not ord('0') <= ord(number) <= ord('9'):
-                raise TypeError()
+        if not self.is_duplicated_number(guess_number):
+            raise TypeError("숫자만 넣어야함")
+
+        if self.is_duplicated_number(guess_number):
+            raise TypeError("중복된 숫자 들어옴")
+
+    def is_duplicated_number(self, guess_number):
+        return guess_number[0] == guess_number[1] or \
+            guess_number[0] == guess_number[2] or \
+            guess_number[2] == guess_number[1]
