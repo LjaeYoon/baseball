06e02f8d69bdf5e53cc0518213d76ea7ab710627
jaeyoon <chris0258@naver.com>
2025-07-31 14:28:15 +0900

    green p.52 refac p.58

--- game.py
+++ game.py
@@ -1,6 +1,24 @@
+from game_result import GameResult
+
+
 class Game:
+
+    def __init__(self):
+        self._question = ""
+
+    @property
+    def question(self):
+        raise AttributeError("읽을 수 없는 송성")
+
+    @question.setter
+    def question(self, question):
+        self._question = question
+
     def guess(self, guess_number):
         self.assert_illegal_value(guess_number)
+        if guess_number == self._question:
+            return GameResult(True, 3, 0)
+        return None
 
     def assert_illegal_value(self, guess_number):
         if guess_number is None:
@@ -9,7 +27,7 @@ class Game:
         if len(guess_number) != 3:
             raise TypeError("입력 문자열 길이가 3 이 아님")
 
-        if not self.is_duplicated_number(guess_number):
+        if not guess_number.isdigit():
             raise TypeError("숫자만 넣어야함")
 
         if self.is_duplicated_number(guess_number):
